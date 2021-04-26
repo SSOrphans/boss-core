f26c2fcccccc92ccff1d0dfcbe4360e59729789f
Derrian Harris <dsh0018au@gmail.com>
2021-04-26 03:31:49 -0500

    SSOR-165 Added Card Entity, Card Type Entity, and Card repository. Refactored Card Type to use enum. Added Card table to h2 schema.

--- src/main/java/org/ssor/boss/core/entity/LoanType.java
+++ src/main/java/org/ssor/boss/core/entity/LoanType.java
@@ -1,40 +1,24 @@
 package org.ssor.boss.core.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 /**
  * @author Derrian Harris
- *
  */
 public enum LoanType {
-	LOAN_STUDENT("Student"),
-	LOAN_PERSONAL("Personal");
+    LOAN_STUDENT("Student"),
+    LOAN_PERSONAL("Personal");
 
-	private String name;
+    private final String name;
 
-	LoanType(String name) {
-		this.name = name;
-	}
+    LoanType(String name) {
+        this.name = name;
+    }
 
-	public int index()
-	{
-		return ordinal();
-	}
+    public int index() {
+        return ordinal();
+    }
 
-	@Override
-	public String toString() {
-		return this.name;
-	}
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
