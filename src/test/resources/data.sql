e39c9a7a7832023770337653bcf249969dcab0fb
Derrian Harris <dsh0018au@gmail.com>
2021-04-30 13:57:32 -0500

    SSOR-165 updated schema.sql

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -4,7 +4,7 @@ values (2, 1, 'SoraKatadzuma', 'sorakatadzuma@gmail.com',
 
 insert into boss.user (type_id, branch_id, username, email, password, created, deleted, enabled, locked)
 values (2, 1, 'Monkey', 'me@example.com', 'D8A4D80f7C27F25067E56671B1AA4f7172E3C7418DE442fDF42fF49CF49FE20E',
-        NOW(), NULL, true, false);
+        0, NULL, true, false);
 
 insert into boss.loan (type_id, user_id, branch_id, amount, interest_rate, taken_at, due_by, amount_due)
 values (0, 1, 1, 50000, 0.02, NOW(), NOW(), 25000);
@@ -15,4 +15,3 @@ values (1, 1, 1, 25000, 0.1, NOW(), NOW(), 5000);
 insert into boss.card (type_id, number_hash, account_id, created, active_since, expiration_date, pin, cvv, confirmed,
                        active, stolen)
 values (0, '1234567890123456', 1, NOW(), NOW(), NOW(), 1111, 111, true, true, false);
-        0, NULL, true, false);
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -27,16 +27,19 @@ create table if not exists boss.card
     active          bit(1)   NOT NULL,
     stolen          bit(1)   NOT NULL,
     PRIMARY KEY (id)
-create table if not exists boss.user (
-   id INT NOT NULL AUTO_INCREMENT UNIQUE,
-   type_id TINYINT NOT NULL,
-   branch_id INT NOT NULL,
-   username VARCHAR(16) NOT NULL UNIQUE,
-   email VARCHAR(128) NOT NULL UNIQUE,
-   password CHAR(64) NOT NULL,
-   created BIGINT NOT NULL,
-   deleted BIGINT,
-   enabled BIT NOT NULL,
-   locked BIT NOT NULL,
-   PRIMARY KEY (id)
 );
+create table if not exists boss.user
+(
+    id        INT          NOT NULL AUTO_INCREMENT UNIQUE,
+    type_id   TINYINT      NOT NULL,
+    branch_id INT          NOT NULL,
+    username  VARCHAR(16)  NOT NULL UNIQUE,
+    email     VARCHAR(128) NOT NULL UNIQUE,
+    password  CHAR(64)     NOT NULL,
+    created   BIGINT       NOT NULL,
+    deleted   BIGINT,
+    enabled   BIT          NOT NULL,
+    locked    BIT          NOT NULL,
+    PRIMARY KEY (id)
+);
+
