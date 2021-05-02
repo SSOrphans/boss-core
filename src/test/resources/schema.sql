15da9cba7c81fed25b32e76b4adb13ed77fe799b
Derrian Harris <80282599+DerrianH@users.noreply.github.com>
2021-05-01 19:11:08 -0500

    Refactor/ssor 165 card submodule (#6)
    * SSOR-166 Added Loan Entity, Loan Type Entity, and Loan repository. Refactor Loan Type to use enum. Added Loan table to h2 schema.
    
    * SSOR-165 Added Card Entity, Card Type Entity, and Card repository. Refactored Card Type to use enum. Added Card table to h2 schema.
    
    * SSOR-165 Added Card Entity, Card Type Entity, and Card repository. Refactored Card Type to use enum. Added Card table to h2 schema.
    
    * SSOR-61 Refactored to use boss-core submodule. Added swagger.
    
    * SSOR-165 Updated pom.
    
    * SSOR-58 Finished refactor
    
    * SSOR-61 Finished refactor
    
    * SSOR-165 updated schema.sql
    
    * SSOR-165 updated pom.xml
    
    Co-authored-by: Derrian Harris <dsh0018au@gmail.com>

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,14 +1,45 @@
 create schema if not exists boss;
-create table if not exists boss.user (
-   id INT NOT NULL AUTO_INCREMENT UNIQUE,
-   type_id TINYINT NOT NULL,
-   branch_id INT NOT NULL,
-   username VARCHAR(16) NOT NULL UNIQUE,
-   email VARCHAR(128) NOT NULL UNIQUE,
-   password CHAR(64) NOT NULL,
-   created BIGINT NOT NULL,
-   deleted BIGINT,
-   enabled BIT NOT NULL,
-   locked BIT NOT NULL,
-   PRIMARY KEY (id)
+create table if not exists boss.loan
+(
+    id            INT      NOT NULL AUTO_INCREMENT UNIQUE,
+    type_id       TINYINT  NOT NULL,
+    user_id       INT      NOT NULL,
+    branch_id     INT      NOT NULL,
+    amount        FLOAT    NOT NULL,
+    interest_rate FLOAT    NOT NULL,
+    taken_at      DATETIME NOT NULL,
+    due_by        DATE     NOT NULL,
+    amount_due    FLOAT    NOT NULL,
+    PRIMARY KEY (id)
 );
+create table if not exists boss.card
+(
+    id              INT      NOT NULL AUTO_INCREMENT UNIQUE,
+    type_id         TINYINT  NOT NULL,
+    number_hash     char(64) NOT NULL,
+    account_id      int      NOT NULL,
+    created         DATETIME NOT NULL,
+    active_since    DATE     NOT NULL,
+    expiration_date DATE     NOT NULL,
+    pin             smallint NOT NULL,
+    cvv             tinyint  NOT NULL,
+    confirmed       bit(1)   NOT NULL,
+    active          bit(1)   NOT NULL,
+    stolen          bit(1)   NOT NULL,
+    PRIMARY KEY (id)
+);
+create table if not exists boss.user
+(
+    id        INT          NOT NULL AUTO_INCREMENT UNIQUE,
+    type_id   TINYINT      NOT NULL,
+    branch_id INT          NOT NULL,
+    username  VARCHAR(16)  NOT NULL UNIQUE,
+    email     VARCHAR(128) NOT NULL UNIQUE,
+    password  CHAR(64)     NOT NULL,
+    created   BIGINT       NOT NULL,
+    deleted   BIGINT,
+    enabled   BIT          NOT NULL,
+    locked    BIT          NOT NULL,
+    PRIMARY KEY (id)
+);
+
