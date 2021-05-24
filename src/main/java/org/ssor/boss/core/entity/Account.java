341ef93ad8148509ca97a46479e134635e4decb1
Bermond Jon Batistiana <bermond.batistiana@smoothstack.com>
2021-05-24 11:15:00 -0700

    SSOR-223 SonarQubeTest: Removed unwanted imports

--- src/main/java/org/ssor/boss/core/entity/Account.java
+++ src/main/java/org/ssor/boss/core/entity/Account.java
@@ -5,7 +5,6 @@ import lombok.*;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Setter
--- src/main/java/org/ssor/boss/core/entity/Transaction.java
+++ src/main/java/org/ssor/boss/core/entity/Transaction.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Entity
 @Table(name = "transaction", schema = "boss")
