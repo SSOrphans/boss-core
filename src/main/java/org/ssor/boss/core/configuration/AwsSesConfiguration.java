c17056610b558decd60f7516814066be28ce035b
Derrian Harris <derrian.harris@smoothstack.com>
2021-06-10 14:28:12 -0500

    SSOR-50 Added password recovery

--- src/main/java/org/ssor/boss/core/configuration/AwsSesConfiguration.java
+++ src/main/java/org/ssor/boss/core/configuration/AwsSesConfiguration.java
@@ -1,5 +1,7 @@
 package org.ssor.boss.core.configuration;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +19,27 @@ public class AwsSesConfiguration {
      */
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return AmazonSimpleEmailServiceClientBuilder.standard()
+        //FIXME: Find a better way to get aws credentials
+        AWSCredentialsProvider credentialsProvider = new AWSCredentialsProvider() {
+            @Override
+            public AWSCredentials getCredentials() {
+                return new AWSCredentials() {
+                    @Override
+                    public String getAWSSecretKey() {
+                        return "x1x57lalNj4jtp2XmlU3nK1cftq+4DPXGX446JpO";
+                    }
+                    @Override
+                    public String getAWSAccessKeyId() {
+                        return "AKIA2DCR3XYBACCVPG2L";
+                    }
+                };
+            }
+
+            @Override
+            public void refresh() {
+            }
+        };
+        return AmazonSimpleEmailServiceClientBuilder.standard().withCredentials(credentialsProvider)
                 .withRegion(REGION).build();
     }
 }
--- src/main/java/org/ssor/boss/core/service/AwsSesService.java
+++ src/main/java/org/ssor/boss/core/service/AwsSesService.java
@@ -5,9 +5,11 @@ import com.amazonaws.services.simpleemail.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.ssor.boss.core.transfer.Email;
 
-@Service
+
 @Slf4j
+@Service
 public class AwsSesService {
 
     private static final String CHAR_SET = "UTF-8";
@@ -15,23 +17,23 @@ public class AwsSesService {
     @Autowired
     private AmazonSimpleEmailService emailService;
 
-    public SendEmailResult sendEmail(String senderEmail,String recieverEmail, String subject, String body) {
+    public SendEmailResult sendEmail(Email mail) {
         try {
             // The time for request/response round trip to aws in milliseconds
             int requestTimeout = 3000;
             SendEmailRequest request = new SendEmailRequest()
                     .withDestination(
-                            new Destination().withToAddresses(recieverEmail))
+                            new Destination().withToAddresses(mail.getRecipient()))
                     .withMessage(new Message()
                             .withBody(new Body()
                                     .withText(new Content()
-                                            .withCharset(CHAR_SET).withData(body)))
+                                            .withCharset(CHAR_SET).withData(mail.getBody())))
                             .withSubject(new Content()
-                                    .withCharset(CHAR_SET).withData(subject)))
-                    .withSource(senderEmail).withSdkRequestTimeout(requestTimeout);
+                                    .withCharset(CHAR_SET).withData(mail.getSubject())))
+                    .withSource(mail.getSender()).withSdkRequestTimeout(requestTimeout);
             return emailService.sendEmail(request);
         } catch (RuntimeException e) {
-            log.error("Error occurred sending email to {} ", recieverEmail, e);
+            log.error("Error occurred sending email to {} ", mail.getRecipient(), e);
             return null;
         }
     }
