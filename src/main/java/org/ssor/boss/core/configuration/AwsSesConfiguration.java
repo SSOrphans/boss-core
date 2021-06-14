b534518a508352992b604c29f937d14e5e140ddc
Derrian Harris <derrian.harris@smoothstack.com>
2021-06-14 11:38:37 -0500

    SSOR-50 Updated SES to use AWS CLI

--- src/main/java/org/ssor/boss/core/configuration/AwsSesConfiguration.java
+++ src/main/java/org/ssor/boss/core/configuration/AwsSesConfiguration.java
@@ -1,7 +1,5 @@
 package org.ssor.boss.core.configuration;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,37 +7,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsSesConfiguration {
-
-    private final String REGION="us-east-2";
-
-    /**
-     * Build the AWS ses client
-     *
-     * @return AmazonSimpleEmailServiceClientBuilder
-     */
+    /*
+    *   Configures AWS SES bean.
+    *   Must have AWS CLI configured for this method to work
+    */
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        //FIXME: Find a better way to get aws credentials
-        AWSCredentialsProvider credentialsProvider = new AWSCredentialsProvider() {
-            @Override
-            public AWSCredentials getCredentials() {
-                return new AWSCredentials() {
-                    @Override
-                    public String getAWSSecretKey() {
-                        return "x1x57lalNj4jtp2XmlU3nK1cftq+4DPXGX446JpO";
-                    }
-                    @Override
-                    public String getAWSAccessKeyId() {
-                        return "AKIA2DCR3XYBACCVPG2L";
-                    }
-                };
-            }
-
-            @Override
-            public void refresh() {
-            }
-        };
-        return AmazonSimpleEmailServiceClientBuilder.standard().withCredentials(credentialsProvider)
-                .withRegion(REGION).build();
+        return AmazonSimpleEmailServiceClientBuilder.defaultClient();
     }
 }
