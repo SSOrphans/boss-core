cfe8493f7d7d8b9e10a6628f0ea3c6a5ead95453
LeftRuleMatters <john.christman@smoothstack.com>
2021-08-17 18:01:00 -0500

    Support for miles programs on accounts

--- src/main/java/org/ssor/boss/core/entity/Account.java
+++ src/main/java/org/ssor/boss/core/entity/Account.java
@@ -1,8 +1,18 @@
 package org.ssor.boss.core.entity;
 
-import lombok.*;
-
-import javax.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 import java.util.List;
@@ -14,53 +24,51 @@ import java.io.Serializable;
  *
  * @author Bermond Jon Batistiana
  */
-@Data
-@Entity
-@Builder
-@AllArgsConstructor
+@Setter
+@Getter
 @NoArgsConstructor
-@Table(name = "account", schema = "boss", uniqueConstraints = {
-    @UniqueConstraint(columnNames = "id")
-})
+@Entity(name = "Account")
+@Table(schema = "boss", name = "account")
+@EqualsAndHashCode(doNotUseGetters = true, onlyExplicitlyIncluded = true)
 public class Account implements Serializable
 {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @EqualsAndHashCode.Include
+  @Column(nullable = false, unique = true, updatable = false)
   private Long id;
+
   @Enumerated
-  @Column(name = "type_id")
+  @Column(name = "type_id", nullable = false)
   private AccountType accountType;
-  @Column(name = "name")
-  private String name;
-  private Float balance;
-  @Column(name = "opened")
-  private LocalDate opened;
-  @Column(name = "closed")
-  private LocalDate closed;
-  @Column(name = "confirmed")
-  private Boolean confirmed;
-  private Boolean active;
-  @Column(name = "branch_id")
+
+  @Column(name = "branch_id", nullable = false)
   private Integer branchId;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private Float balance;
+
+  @Column(nullable = false)
+  private LocalDate opened;
+
+  @Column(insertable = false)
+  private LocalDate closed;
+
+  @Column(nullable = false, updatable = false)
+  private Boolean confirmed;
+
+  @Column(nullable = false)
+  private Boolean active;
+
+  @OneToOne
+  @JoinTable(schema = "boss", name = "account_miles_programs", joinColumns = @JoinColumn(name = "account_id"),
+             inverseJoinColumns = @JoinColumn(name = "miles_program_id"))
+  private MilesProgramEntity milesProgram;
 
   @OneToMany
   @JoinTable(name = "account_users", schema = "boss", joinColumns = @JoinColumn(name = "account_id"),
              inverseJoinColumns = @JoinColumn(name = "user_id"))
   private List<User> users;
-
-  @Override
-  public boolean equals(Object o)
-  {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Account account = (Account) o;
-    return id.equals(account.id);
-  }
-
-  @Override
-  public int hashCode()
-  {
-    return Objects.hash(id);
-  }
 }
